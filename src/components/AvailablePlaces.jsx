ae42123d9516a5b8b84a5816647db4387df09560
Abhishek Panchal <abhishekpanchal8894@gmail.com>
2025-12-27 17:33:23 +0530

    transforming the fetched data: sort the places by user's coordinates

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,23 +1,42 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 import ErrorPage from './ErrorPage.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [error, setError] = useState();
 
+  function getUserLocation() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          resolve(position);
+        },
+        (err) => reject(err)
+      );
+    });
+  }
+
   useEffect(() => {
     return async () => {
       setIsFetching(true);
       try {
-        const response = await fetch('http://localhost:3000/place');
+        const response = await fetch('http://localhost:3000/places');
         const { places } = await response.json();
         if (!response.ok) {
           const error = new Error('Failed to fetch places');
           throw error;
         }
-        setAvailablePlaces(places);
+
+        const {
+          coords: { latitude, longitude },
+        } = await getUserLocation();
+
+        const sortedPlaces = sortPlacesByDistance(places, latitude, longitude);
+
+        setAvailablePlaces(sortedPlaces);
       } catch (error) {
         setError({
           message:
